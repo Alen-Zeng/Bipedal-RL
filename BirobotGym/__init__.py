5662d266fdb992405839477ac7722c42a68ee4c5
zxl <chenmoshao@gmail.com>
2024-06-16 20:17:36 +0800

    更新最大step数

--- BirobotGym/__init__.py
+++ BirobotGym/__init__.py
@@ -3,5 +3,5 @@ from gymnasium.envs.registration import register
 register(
     id="Birobot-v0",
     entry_point="BirobotGym.env:Birobot",
-    max_episode_steps=15000,
+    max_episode_steps=150000,
 )
